52efed24dd003621b1016eec5ee0e7b6bae4cbfb
peninah98 <peninangizwena@gmail.com>
2023-08-17 08:32:55 +0200

    TypeScipt Tutorials

---
+++ test.js
@@ -0,0 +1,5 @@
+"use strict";
+function add(firstValue, secondValue) {
+    return firstValue + secondValue;
+}
+console.log(add(12, 24));
---
+++ test.ts
@@ -0,0 +1,5 @@
+function add(firstValue: number, secondValue: number): number {
+    return firstValue  + secondValue
+}
+
+console.log(add(12, 24));
