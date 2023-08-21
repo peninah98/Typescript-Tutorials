ebf8af54990c837215c8b6571bc57051e9d930af
peninah98 <peninangizwena@gmail.com>
2023-08-21 10:59:55 +0200

    feat : ft/tuples readonly keyword on arrar

---
+++ dist/readOnly.js
@@ -0,0 +1,8 @@
+"use strict";
+const arr = [1, 2, 3, 4, 5, 6, 6, 7, 6, 8];
+function square(numbers) {
+    return numbers.map((num) => Math.pow(num, 2));
+}
+console.log(square(arr));
+const names = ["Kalisa", "Kamaliza", "Karagwa"];
+console.log(names);
